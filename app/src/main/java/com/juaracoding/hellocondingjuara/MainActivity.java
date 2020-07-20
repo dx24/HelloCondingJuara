b90c47e39129d9682ecf73c059e0e71674f8f9e2
dx24 <danz5566@gmail.com>
2020-07-20 17:37:55 +0700

    Initial commit

---
+++ app/src/main/java/com/juaracoding/hellocondingjuara/MainActivity.java
@@ -0,0 +1,34 @@
+package com.juaracoding.hellocondingjuara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsername,txtPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        txtUsername.setText("Daniel");
+
+
+    }
+
+    public void login (View view){
+        String pesan = "Isinya adalah " + txtUsername.getText().toString() + " passwordnya adalah" +txtPassword.getText().toString();
+        Toast.makeText(MainActivity.this,pesan,Toast.LENGTH_LONG).show();
+    }
+}
